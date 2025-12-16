f01f73ea5ed670acc8ef24e2ef57b7dbc9912a2f
abdulrahman0000001 <thabet77507@gmail.com>
2025-12-16 21:27:59 +0200

    Update problem1.js

--- js/projects/problem1.js
+++ js/projects/problem1.js
@@ -1,1 +1,9 @@
-
+let x = 5; let y = 4;
+console.log(x + y);
+console.log(x - y);
+console.log((x*y) + 3);
+console.log(x + y*2);
+consoele.log((x+y) * 2)
+console.log(x / y);
+console.log(x % y);
+console.log((x - 2)*(y + 1))
