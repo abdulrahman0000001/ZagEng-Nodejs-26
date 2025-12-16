56cc600fc2f1d0fcd1e84bf0030a8831dd8f7c3b
abdulrahman0000001 <thabet77507@gmail.com>
2025-12-16 21:51:26 +0200

    Add console logs for various string and number outputs

--- js/projects/problem5.js
+++ js/projects/problem5.js
@@ -1,1 +1,7 @@
-
+let num = 20; let str = " years old";
+console.log(num + str);
+console.log("Age: " + num);
+console.log("Result = " + (num + 5));
+console.log("20" + 10);
+console.log((20 + 10) + " days");
+console.log("JS" + " " + "Course");
